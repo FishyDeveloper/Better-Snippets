d36b8683d83957dacbde023ba9104bbca0470a45
FishyDeveloper <ikanfugu511@gmail.com>
2021-08-29 05:51:02 +0700

    FileChooserPopUp class

--- App.py
+++ App.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.filechooser import FileChooser
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.config import Config
 import pyautogui
@@ -8,6 +10,10 @@ import time
 Config.set('graphics','width',500)
 Config.set('graphics','height',400)
 Config.set('graphics','resizable',False)
+
+class FileChooserPopUp(Screen):
+    SelectedFile = ObjectProperty(None)
+
 
 class FirstScreen(Screen):
     pass
