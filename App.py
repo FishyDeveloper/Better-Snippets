d8919e2e4fe15b439375931267b31b4519332370
FishyDeveloper <ikanfugu511@gmail.com>
2021-09-10 17:38:19 +0700

    Using tkinter now

--- App.py
+++ App.py
@@ -1,34 +1,8 @@
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.properties import ObjectProperty
-from kivy.uix.filechooser import FileChooser
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.config import Config
-import pyautogui
-import time
+from tkinter import *
 
-Config.set('graphics','width',500)
-Config.set('graphics','height',400)
-Config.set('graphics','resizable',False)
-
-class FileChooserPopUp(Screen):
-    SelectedFile = ObjectProperty(None)
+root = Tk()
+root.title('DekstopSS')
 
 
-class FirstScreen(Screen):
-    def toggle_theme(self):
-        print('works?')
 
-class SecondScreen(Screen):
-    pass
-
-class WindowManager(ScreenManager):
-    pass
-
-kv = Builder.load_file('App.kv')
-
-class whatever(App):
-    def build(self):
-        return kv
-
-whatever().run()
+root.mainloop()
